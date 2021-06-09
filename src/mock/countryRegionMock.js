3e3db5dc741628ffc1e47ef7d627ef81bdbef21b
TejasriNiharika22 <84013826+TejasriNiharika22@users.noreply.github.com>
2021-06-09 12:51:57 +0530

    Add files via upload

---
+++ src/mock/countryRegionMock.js
@@ -0,0 +1,24 @@
+const MockContent = {
+    heading: "Countries App",
+    regionDropDown:[
+        {
+            id:"africa",
+            name:"Africa"
+        },
+        {
+            id:"americas",
+            name:"Americas"
+        },
+        {
+            id:"asia",
+            name:"Asia"
+        },
+        {
+            id:"europe",
+            name:"Europe"
+        }
+    ],
+    regionDropdownLabel: "Region",
+    countryDropdownLabel: "Country"
+}
+export default MockContent;
